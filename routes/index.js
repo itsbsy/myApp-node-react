424fe046aa707622e80a48bbff02188fc96c42ee
itsbsy <brijeshsinghyadav.9@gmail.com>
2024-01-30 18:19:21 +0530

    CRUD completed

--- routes/index.js
+++ routes/index.js
@@ -25,14 +25,37 @@ router.get('/user', async(req, res) =>{
         console.log(error)
     }
 })
-router.post('/user', async (req, res) => {
+router.patch('/updateuser', async (req, res) => {
    try {
-    let params = req.body
-   const newData = await User.create(params);
-    res.json(newData)
+    let params = req.query.id
+    let dataToBeUpdated = req.body 
+    console.log(dataToBeUpdated)
+    const result = await User.updateOne({ _id: params }, { $set: dataToBeUpdated });
+    res.json(result)
     
    } catch (error) {
      console.log(error)
    }
-})
+});
+router.delete('/deleteuser', async (req, res) => {
+    try {
+     let params = req.query.id
+     const result = await User.deleteOne({ _id: params });
+     res.json(result)
+     
+    } catch (error) {
+      console.log(error)
+    }
+ });
+
+router.post('/user', async (req, res) => {
+    try {
+    let params = req.body
+    const newData = await User.create(params);
+     res.json(newData)
+     
+    } catch (error) {
+      console.log(error)
+    }
+ })
 module.exports = router;
